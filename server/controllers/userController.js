71404a5a94706cebfded9fac020505c907dfb2f5
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-24 02:09:48 +0300

    User sign up controller function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,8 @@
+const bcrypt = require("bcryptjs");
+
+const createError = require("../utils/createError");
+const User = require("../models/userModel");
+
 // @desc   Auth user/set token
 // route   POST /api/users/auth
 // @access Public
@@ -9,9 +14,33 @@ exports.authUser = async (req, res) => {
 // route   POST /api/users
 // @access Public
 
-exports.registerUser = async (req, res) => {
+exports.registerUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    next(err);
+  }
+  if (existingUser) return next(createError(404, "User already exists!"));
+
+  try {
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json({ message: "User created!", user: savedUser });
+  } catch (err) {
+    next(err);
+  }
+
   res.status(200).json({ message: "Register User" });
 };
+
 // @desc   Logout a user
 // route   POST /api/users/logout
 // @access Public
