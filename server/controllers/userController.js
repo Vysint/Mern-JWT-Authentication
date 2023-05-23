95fa7d75467673c7e6dd5f865a33fe7cea72a625
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-23 18:17:40 +0300

    Routes connected to the controller functions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,3 +5,31 @@
 exports.authUser = async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 };
+// @desc   Register a new user
+// route   POST /api/users
+// @access Public
+
+exports.registerUser = async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+};
+// @desc   Logout a user
+// route   POST /api/users/logout
+// @access Public
+
+exports.logoutUser = async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+};
+// @desc   GET user profile
+// route   GET /api/users/profile
+// @access private
+
+exports.getUserProfile = async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+};
+// @desc   Update user profile
+// route   PUT /api/users/profile
+// @access private
+
+exports.updateUserProfile = async (req, res) => {
+  res.status(200).json({ message: "User Profile updated!" });
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
-const { authUser } = require("../controllers/userController");
+const {
+  authUser,
+  logoutUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 module.exports = router;
