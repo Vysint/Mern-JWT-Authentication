34d4ce226a1ca687044462baff11d211e9b55d03
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-24 13:21:33 +0300

    Updated user registration function controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,7 +33,13 @@ exports.registerUser = async (req, res, next) => {
       password: hashedPassword,
     });
     const savedUser = await newUser.save();
-    res.status(201).json({ message: "User created!", user: savedUser });
+    res
+      .status(201)
+      .json({
+        _id: savedUser._id,
+        name: savedUser.name,
+        email: savedUser.email,
+      });
   } catch (err) {
     next(err);
   }
