759b4da97697d2b4f21600f144ba4897f4efd794
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-25 02:06:18 +0300

    Token verification

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_KEY, { expiresIn: "30d" });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
