6c3617ae1165afa2d8097a7213b65ded19685a68
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-26 01:26:29 +0300

    Added navbar

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link className="link" to="/">
+            <span className="text">MERN APP</span>
+          </Link>
+          <span className="dot">.</span>
+        </div>
+        <div className="navItems">
+          <ul className="menu">
+            <li>
+              <Link to="/" className="link item">
+                <FaSignInAlt />
+                <span>Sign In</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="link item">
+                <FaSignOutAlt />
+                <span>Sign Up</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
