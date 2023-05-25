c60b8b379324d17764bbd8b4a56e2fbc49fe9d4e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-26 01:27:04 +0300

    Attached Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
+import Header from "./components/header/Header";
 
 const App = () => {
   return (
     <>
-    <h1>Mern Auth</h1>
+      <Header />
+      <h1>Mern Auth</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
