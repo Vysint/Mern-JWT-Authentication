3486e4b8e42bbf43c0004e1efe9febeafe6fba44
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-26 23:57:27 +0300

    Re-configured routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {
+  createRoutesFromElements,
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+} from "react-router-dom";
+import Homescreen from "./screens/Homescreen.jsx";
 import "./index.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Homescreen />} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
