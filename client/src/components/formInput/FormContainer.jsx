af5576dff8b90466f6e896238b21693f05b0806c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-28 13:28:34 +0300

    Input prop component

---
+++ client/src/components/formInput/FormContainer.jsx
@@ -0,0 +1,11 @@
+const FormContainer = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FormContainer;
