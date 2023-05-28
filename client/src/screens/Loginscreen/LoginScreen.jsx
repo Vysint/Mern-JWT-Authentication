3bfa834a7f34e58177fcff57335568c5fd3fa1e9
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-28 12:25:24 +0300

    Login Page

---
+++ client/src/screens/Loginscreen/LoginScreen.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import FormContainer from "../../components/formInput/FormContainer";
+import { Link } from "react-router-dom";
+
+import "./LoginScreen.css";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <form onSubmit={submitHandler}>
+        <div className="inputs">
+          <label>Email Address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="inputs">
+          <label>Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" style={{ background: "rgb(56, 118, 241)" }}>
+          Sign In
+        </button>
+        <div className="register">
+          <span>New Customer?</span>
+          <Link to="/register">Register</Link>
+        </div>
+      </form>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
