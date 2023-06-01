2c1c8bbae7fe873060d7047f7074242653b492bf
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-02 01:42:48 +0300

    Menu dropdown markup

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
-import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { FaSignInAlt, FaSignOutAlt, FaUserAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import Vincent from "../../assets/Langat.jpeg";
 
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const userInfo = useSelector((state) => state.auth);
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="navbar">
       <div className="container">
@@ -14,20 +20,43 @@ const Header = () => {
           <span className="dot">.</span>
         </div>
         <div className="navItems">
-          <ul className="menu">
-            <li>
-              <Link to="/login" className="link item">
-                <FaSignInAlt />
-                <span>Sign In</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/register" className="link item">
-                <FaSignOutAlt />
-                <span>Sign Up</span>
-              </Link>
-            </li>
-          </ul>
+          {userInfo ? (
+            <div className="menu-container">
+              <div className="menu">
+                <span className="link item">{userInfo.userInfo.name}</span>
+                <img src={Vincent} onClick={() => setOpen(!open)} />
+              </div>
+              <div className={`menu dropdown ${open ? "active" : "inactive"}`}>
+                <li>
+                  <Link to="/profile" className="userLink">
+                    <FaUserAlt />
+                    <span>Profile</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/logout" className="userLink">
+                    <FaSignOutAlt />
+                    <span>Logout</span>
+                  </Link>
+                </li>
+              </div>
+            </div>
+          ) : (
+            <ul className="menu">
+              <li>
+                <Link to="/login" className="link item">
+                  <FaSignInAlt />
+                  <span>Sign In</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/register" className="link item">
+                  <FaSignOutAlt />
+                  <span>Sign Up</span>
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </div>
