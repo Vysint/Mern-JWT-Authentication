032c1fd204649694f7a7c47224fd243de1db149b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-05 00:10:49 +0300

    Added update user markup

--- client/src/screens/profileScreen/ProfileScreen.jsx
+++ client/src/screens/profileScreen/ProfileScreen.jsx
@@ -1,9 +1,82 @@
-
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { setCredentials } from "../../slices/authSlice";
 
 const ProfileScreen = () => {
-  return (
-    <div>ProfileScreen</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export default ProfileScreen
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const userInfo = useSelector((state) => state.auth.userInfo);
+
+  useEffect(() => {
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+  }, [userInfo.name, userInfo.email]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      console.log("submit");
+    }
+  };
+  return (
+    <div className="formInputs">
+      <h1>Update Profile</h1>
+      <form onSubmit={submitHandler}>
+        <div className="inputs">
+          <label>Name</label>
+          <input
+            type="text"
+            id="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>Email Address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            id="confirmpassword"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="button-submit"
+          style={{ background: "rgb(56, 118, 241)" }}
+        >
+          Update
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileScreen;
