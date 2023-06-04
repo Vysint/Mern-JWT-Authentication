b328b6c70c1ce8634518d46cb7b9f08ae2a8039b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-05 00:21:32 +0300

    Update User RTK Query data fetching

--- client/src/screens/profileScreen/ProfileScreen.jsx
+++ client/src/screens/profileScreen/ProfileScreen.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { useUpdateUserMutation } from "../../slices/usersApiSlice";
