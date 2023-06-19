df49966d8c037ebac9fc09a3430c61007c68e5ca
Vincent Kipngetich <vinniebluz@icloud.com>
2023-06-19 17:08:54 +0300

    Code Markup correction

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,27 +1,28 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import "./Hero.css";
 const Hero = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   return (
-    <div className="hero">
-      <div className="container">
-        <div className="card">
-          <h2>MERN Authentication</h2>
-          <p>
-            This is a boilerplate for MERN authentication that stores a JWT in
-            an HTTP-Only cookie. It also uses Redux Toolkit to Manage state.
-          </p>
-          <div className="hero-items">
-            <button>
-              <Link to="/login" className="link one">
-                Sign In
-              </Link>
-            </button>
-            <button>
-              <Link to="/register" className="link two">
-                  Sign Up
-              </Link>
-            </button>
-          </div>
+    <div className="hero-container">
+      <div className="card">
+        <h2>MERN Authentication</h2>
+        <p>
+          This is a boilerplate for MERN authentication that stores a JWT in an
+          HTTP-Only cookie. It also uses Redux Toolkit to Manage state.
+        </p>
+        <div className="hero-items">
+          <button>
+            <Link to="/login" className="link one">
+              Sign In
+            </Link>
+          </button>
+          <button>
+            <Link to="/register" className="link two">
+              Sign Up
+            </Link>
+          </button>
         </div>
       </div>
     </div>
